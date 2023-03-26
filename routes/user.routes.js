7effa6b598320bb3087a4c38625bbe7a01bb011a
amanc1248 <amanc1248@gmail.com>
2023-03-26 22:00:23 +0545

    Add: changeUserPassword route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,10 +6,11 @@ const {
   validateUserPassword,
   generateJwtToken,
   checkIfUserExistsForResetingPassword,
-  changeUserPassword,
+  resetUserPassword,
   sendUserDetails,
   udpateUserEmail,
   generateJWTWhenChangedEmail,
+  changeUserPassword,
 } = require("../controllers/user/user.controller");
 const { authenticateToken } = require("../middlewares/auth.middlewares");
 const { sendEmail } = require("../utils/email.util");
@@ -21,7 +22,8 @@ router.post("/", checkUserIfExists, createUser);
 
 router.post("/sendEmail", sendEmail);
 router.get("/login", userLogin, validateUserPassword, generateJwtToken);
-router.put("/resetPassword",checkIfUserExistsForResetingPassword,changeUserPassword);
+router.put("/resetPassword",checkIfUserExistsForResetingPassword,resetUserPassword);
 router.get("/checkLogin", authenticateToken, sendUserDetails);
-router.put("/changeEmail", authenticateToken, udpateUserEmail, generateJWTWhenChangedEmail)
+router.put("/changeEmail", authenticateToken, udpateUserEmail, generateJWTWhenChangedEmail);
+router.put("/changePassword", authenticateToken, changeUserPassword)
 module.exports = router;
