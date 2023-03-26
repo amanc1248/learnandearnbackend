a0c5b730d3c891efc4c5c9f3dd887154f0b2e47f
amanc1248 <amanc1248@gmail.com>
2023-03-26 13:27:16 +0545

    Add: security fields in user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,23 +1,39 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      optional: true,
+    },
+    collectionPermissions: {
+      type: [
+        {
+          collectionName: String,
+          permissions: [String],
+        },
+      ],
+      optional: true,
+    },
+    role: {
+      type: String,
+      optional: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password:{
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: Number,
-    optional: true,
-  },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
