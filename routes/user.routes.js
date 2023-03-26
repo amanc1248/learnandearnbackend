746b9c67c147669165399c2900ab95750199c479
amanc1248 <amanc1248@gmail.com>
2023-03-26 13:26:56 +0545

    Add: changeEmail route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,6 +8,8 @@ const {
   checkIfUserExistsForResetingPassword,
   changeUserPassword,
   sendUserDetails,
+  udpateUserEmail,
+  generateJWTWhenChangedEmail,
 } = require("../controllers/user/user.controller");
 const { authenticateToken } = require("../middlewares/auth.middlewares");
 const { sendEmail } = require("../utils/email.util");
@@ -21,4 +23,5 @@ router.post("/sendEmail", sendEmail);
 router.get("/login", userLogin, validateUserPassword, generateJwtToken);
 router.put("/resetPassword",checkIfUserExistsForResetingPassword,changeUserPassword);
 router.get("/checkLogin", authenticateToken, sendUserDetails);
+router.put("/changeEmail", authenticateToken, udpateUserEmail, generateJWTWhenChangedEmail)
 module.exports = router;
