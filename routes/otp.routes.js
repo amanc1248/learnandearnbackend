225248013182c3565694b776e7d12fd3ad4459b4
amanc1248 <amanc1248@gmail.com>
2023-03-26 13:26:43 +0545

    Add: changeEmailOTP route

--- routes/otp.routes.js
+++ routes/otp.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { sendOTP, saveOTP, getOTP, verifyOTP } = require("../controllers/otp/otp.controller");
 const { checkIfuserExistsBeforeSendingOTP, checkIfUserExistsForResetingPassword } = require("../controllers/user/user.controller");
+const { authenticateToken } = require("../middlewares/auth.middlewares");
 const router = express.Router();
 
 router
   .post("/creatUserOTP", checkIfuserExistsBeforeSendingOTP, sendOTP, saveOTP)
   .get("/", getOTP, verifyOTP)
   .post("/resetPasswordOTP",checkIfUserExistsForResetingPassword, sendOTP, saveOTP)
+  .post("/changeEmailOTP", authenticateToken, sendOTP, saveOTP)
 module.exports = router;
